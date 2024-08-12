b94332b8e55110f448ecd0d9f77dfa4a0261aa5c
Alexey <test@git.ru>
2024-08-12 11:57:46 +0300

    business derections

--- src/app/Http/Controllers/BusinessDirectionController.php
+++ src/app/Http/Controllers/BusinessDirectionController.php
@@ -50,12 +50,10 @@ class BusinessDirectionController extends Controller
         $data = $request->validated();
         $data['user_id'] = auth('sanctum')->id();
 
-        // Найти максимальное значение position среди элементов с тем же parent_id
         $maxPosition = BusinessDirection::where('parent_id', $data['parent_id'] ?? null)
             ->where('user_id', $data['user_id'])
             ->max('position');
 
-        // Установить значение position как maxPosition + 1
         $data['position'] = $maxPosition !== null ? $maxPosition + 1 : 0;
 
         $direction = BusinessDirection::create($data);
